300b6548f215dc1ae28f6a6ddffa61badc4a54d8
Nicomedinapozzi <106574241+Nicomedinapozzi@users.noreply.github.com>
2022-07-20 19:43:02 -0300

    Update main.js

--- Desafio 2 Arrays/JS/main.js
+++ Desafio 2 Arrays/JS/main.js
@@ -2,34 +2,59 @@ let pacientes = []
 
 
 class Paciente {
-    constructor (nombre, apellido,dni,histclin){
+    constructor(nombre, apellido, dni, histclin) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
         this.histclin = histclin;
     }
 }
-let continuar = "Y"
-cargardatospte()
-function cargardatospte (){
-    let nombre = prompt("Ingrese Nombre")
-    let apellido = prompt("Ingrese apellido")
-    let dni = prompt("Ingrese DNI")
-    let histclin = prompt("Ingrese Historia Clinica")
+let continuar = " "
 
-    let paciente= new Paciente (nombre, apellido, dni, histclin)
-    pacientes.push(paciente)
-    
-    continuar = prompt("Seguir Ingresando? Y/N")      
+mostrarOpciones()
+
+function mostrarOpciones(){
+    let opcion = prompt("A-Ingresar// B-Buscar//C-Salir")
+
+    switch (opcion){
+        case "A":
+            continuar = "Y"
+            cargardatospte();
+            break;
+        
+        case "B":
+            console.log(pacientes);
+            mostrarOpciones();
+            break;
+
+        case "C":
+            alert("Muchas Gracias")
+            break;
+
+        default:
+            alert("Opcion no valida")
+            mostrarOpciones();
+            break;
+            
+    }
 }
 
-if (continuar === "Y"){
-    cargardatospte()
-}    
-else{
-    alert("Gracias")
-    console.log(pacientes)
+
+function cargardatospte() {
+    while (continuar === "Y") {
+        let nombre = prompt("Ingrese Nombre")
+        let apellido = prompt("Ingrese apellido")
+        let dni = prompt("Ingrese DNI")
+        let histclin = prompt("Ingrese Historia Clinica")
+
+        let paciente = new Paciente(nombre, apellido, dni, histclin)
+        pacientes.push(paciente)
+
+        continuar = prompt("Seguir Ingresando? Y/N")
+    }
+    mostrarOpciones()
 }
+
 
 
 
